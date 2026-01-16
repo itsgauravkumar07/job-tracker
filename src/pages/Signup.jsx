2c21e34f9bb5be9f13f99c55e53b391d05a4d3eb
Gaurav kumar <01itsgauravpal@gmail.com>
2026-01-17 00:15:50 +0530

    Add signup form with controlled inputs and validation

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,55 @@
-export default function Siginup(){
+import { useState } from "react"
+
+export default function Signup(){
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
+
+    function handleSignup(e){
+        e.preventDefault();
+
+        const err = {};
+
+         if(name.trim() === ""){
+            err.name = "Name required"
+        }
+
+        if(email.trim() === ""){
+            err.email = "Email required"
+        }
+        if(password.trim() === ""){
+            err.password = "Password required"
+        }
+        setErrors(err);
+
+        if(Object.keys(err).length > 0) return;
+
+        console.log("Name: ", name);
+        console.log("email: ", email);
+        console.log("password" , password);
+
+        setName("");
+        setEmail("");
+        setPassword("");
+    }
 
     return(
-        <div>
-            <h1>Signup page</h1>
+        <div className="flex justify-center items-center h-screen">
+            <div className=" flex flex-col border border-gray-300 p-8 rounded-2xl w-2xs">
+                <h2 className="text-lg font-bold">Sign up with email</h2>
+                <form onSubmit={handleSignup} className="flex flex-col mt-5 gap-2.5">
+                    <input type="text" placeholder="name" value={name} onChange={e => setName(e.target.value)} className="bg-gray-200 rounded-md p-1 pl-4 hover:bg-gray-300"/>
+                    {errors.name && <p className="text-red-300">{errors.name}</p>}
+                    <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} className="bg-gray-200 rounded-md p-1 pl-4 hover:bg-gray-300"/>
+                    {errors.email && <p className="text-red-300">{errors.email}</p>}
+                    <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} className="bg-gray-200 rounded-md p-1 pl-4 hover:bg-gray-300"/>
+                     {errors.password && <p className="text-red-300">{errors.password}</p>}
+                    <button className="bg-black rounded-md p-1 text-white hover:bg-white hover:text-black hover:border hover:border-gray-300 hover:font-bold">Sign up</button>
+                </form>
+            </div>
+            
         </div>
     )
 }
